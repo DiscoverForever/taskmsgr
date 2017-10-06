42b4c0b2a6e3b235678c34b5fd518debecd1ea12
tangyy <tangyueyang@lztogether.com>
2017-10-06 16:01:30 +0800

    Mod 引入modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MdToolbarModule } from '@angular/material';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MdToolbarModule
   ],
   exports: [
     HeaderComponent, FooterComponent, SidebarComponent
