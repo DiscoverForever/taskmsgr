c9d75d2f1f0344f0ce7a3b027cb29ace60613bbc
tangyy <tangyueyang@lztogether.com>
2017-11-30 17:47:31 +0800

    Add task components

---
+++ src/app/task/task-home/task-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-home',
+  templateUrl: './task-home.component.html',
+  styleUrls: ['./task-home.component.scss']
+})
+export class TaskHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskHomeComponent } from './task-home/task-home.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskHeaderComponent } from './task-header/task-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [TaskHomeComponent, TaskListComponent, TaskItemComponent, TaskHeaderComponent]
 })
 export class TaskModule { }
